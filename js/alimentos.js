63c47a2f87cea038d5609912aec654001972996f
Larissa Barra <lari_conde94@hotmail.com>
2017-05-21 18:11:33 -0300

    Tela de lista de alimentos

---
+++ js/alimentos.js
@@ -0,0 +1,122 @@
+var alimentos = {
+  "norte": {
+    "frutas": {
+      "Abiu": {},
+      "acai": {
+        "nome": "Açaí",
+        "origem": "Amazônia",
+        "descricao": "Na região amazônica, o açaí exerce um importante papel socioeconômico e cultural, pois a bebida obtida a partir de seus frutos tem consumo regional elevado e sua exportação tem aumentado muito nestes últimos anos (2). No estado do Maranhão, é conhecido como Jussara.",
+        "energia": 262,
+        "ptn": 3.6,
+        "lip": 2,
+        "carb": 57.4,
+        "fibra": 32.7,
+        "calcio": 118,
+        "fosforo": 58,
+        "ferro": 1.09,
+        "retinol": 11.8,
+        "b1": 0.36,
+        "b2": 0.01,
+        "niacina": 0.4,
+        "c": 9
+      },
+      "Ajuru": {},
+      "Araçá": {},
+      "Bacaba": {},
+      "Bacuri": {},
+      "Banana-pacovã": {},
+      "Biribá": {},
+      "Buriti": {},
+      "Camapu": {},
+      "Camu-camu": {},
+      "Camutim": {},
+      "Castanha do Brasil": {},
+      "Cubiu": {},
+      "Cupuaçu": {},
+      "Cupuí": {},
+      "Cutite": {},
+      "Guaraná": {},
+      "Inajá": {},
+      "Ingá": {},
+      "Jambo": {},
+      "Manga": {},
+      "Mangaba": {},
+      "Maracujá": {},
+      "Marajá": {},
+      "Murta": {},
+      "Piquiá": {},
+      "Pupunha": {},
+      "Sapota-do-Solimões": {},
+      "Sorva": {},
+      "Taperebá": {},
+      "Tucumã": {},
+      "Umari": {},
+      "Uxi": {}
+    },
+    "hortaliças": {
+
+    },
+    "leguminosas": {
+
+    },
+    "tuberculos": {
+
+    }
+  },
+  "nordeste": {
+    "frutas": {
+
+    },
+    "hortaliças": {
+
+    },
+    "leguminosas": {
+
+    },
+    "tuberculos": {
+
+    }
+  },
+  "sudeste": {
+    "frutas": {
+
+    },
+    "hortaliças": {
+
+    },
+    "leguminosas": {
+
+    },
+    "tuberculos": {
+
+    }
+  },
+  "sul": {
+    "frutas": {
+
+    },
+    "hortaliças": {
+
+    },
+    "leguminosas": {
+
+    },
+    "tuberculos": {
+
+    }
+  },
+  "centrooeste": {
+    "frutas": {
+
+    },
+    "hortaliças": {
+
+    },
+    "leguminosas": {
+
+    },
+    "tuberculos": {
+
+    }
+  }
+};
--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
 // JS aqui.
+$(function() {
+  $('select').change(function() {
+    var regiao = $('#regiao').val();
+    var tipo = $('#tipo').val();
+    var data = alimentos[regiao][tipo];
+
+    var items = [];
+    $.each( data, function( key, val ) {
+      var nome = val.nome ? val.nome : key;
+      items.push( "<a href='#' data-nome='" + key + "' class='list-group-item'>" + nome + "</a>" );
+    });
+    $('#lista').html(items.join( "" ));
+  });
+
+  $('select').trigger('change');
+
+  $( "a.list-group-item" ).click(function( event ) {
+    event.preventDefault();
+    var regiao = $('#regiao').val();
+    var tipo = $('#tipo').val();
+    var nome = $(this).data('nome');
+
+    var alimento = alimentos[regiao][tipo][nome];
+    if (!$.isEmptyObject(alimento)) {
+      $('#nome').html(alimento.nome);
+      $('#origem').html('Origem: ' + alimento.origem);
+      $('#descricao').html(alimento.descricao);
+      $('#energia').html(alimento.energia);
+      $('#ptn').html(alimento.ptn);
+      $('#lip').html(alimento.lip);
+      $('#carb').html(alimento.carb);
+      $('#fibra').html(alimento.fibra);
+      $('#calcio').html(alimento.calcio);
+      $('#fosforo').html(alimento.fosforo);
+      $('#ferro').html(alimento.ferro);
+      $('#retinol').html(alimento.retinol);
+      $('#b1').html(alimento.b1);
+      $('#b2').html(alimento.b2);
+      $('#niacina').html(alimento.niacina);
+      $('#c').html(alimento.c);
+      $('#informacao-nutricional').css('display', 'block');
+    } else {
+      $('#nome').html(nome);
+      $('#origem').html('');
+      $('#descricao').html('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+      $('#informacao-nutricional').css('display', 'none');
+    }
+  });
+});
